54b7c9ce7c3515f0b2b6c877337b0cd13c12232f
Diego Rueda <ing.diegorueda@gmail.com>
2017-10-24 22:58:33 -0500

    create video slicer

---
+++ VideoSlicer/VideoSlicer.py
@@ -0,0 +1,43 @@
+import os
+import cv2
+import sys
+
+# Get input from user
+video_source = sys.argv[1]
+output_folder = sys.argv[2]
+skip_frame = int(sys.argv[3])
+
+
+# Get video source
+video_capture = cv2.VideoCapture(video_source)
+video_capture.set(cv2.CAP_PROP_FPS, 5)
+
+# Create output folder
+if not os.path.exists(sys.argv[2]):
+	os.mkdir(output_folder)
+
+
+count = 0
+
+# Lets read the video
+while(video_capture.isOpened()):
+	ret, frame = video_capture.read()
+	
+	# increment counter
+	count += 1
+
+	# if frame exists
+	if ret == True:
+		# save current frame
+		if count % skip_frame == 0:
+			cv2.imwrite("%s/%s.jpg" % (output_folder, count), frame)
+	else:
+		break
+
+	# ESC to stop
+	key = cv2.waitKey(7) % 0x100
+	if key == 27:
+		break
+
+cv2.destroyAllWindows()
+
